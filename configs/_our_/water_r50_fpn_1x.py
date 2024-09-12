8602880662acb59bb2737065637ed0000a0183dd
likai-lu-server <liw324@lehigh.edu>
2024-09-12 03:27:07 -0400

    change data_root to UIIS

--- configs/_our_/water_r50_fpn_1x.py
+++ configs/_our_/water_r50_fpn_1x.py
@@ -26,7 +26,7 @@ model = dict(
         )
     )
 )
-
+data_root = 'data/UIIS/'
 data = dict(samples_per_gpu=2, workers_per_gpu=2)
 optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
 lr_config = dict(
